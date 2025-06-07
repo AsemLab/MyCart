11e01865f058183a0cf90deba7f1f0c4e5e679e6
Asem Abu alrub <asem.m.abualrub@gmail.com>
2025-06-07 19:54:03 +0300

    Add Bottom Navigation Bar

--- app/src/main/java/com/asemlab/mycart/MainActivity.kt
+++ app/src/main/java/com/asemlab/mycart/MainActivity.kt
@@ -5,45 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.asemlab.mycart.ui.navigation.Destination
+import com.asemlab.mycart.ui.navigation.MyCartBottomBar
+import com.asemlab.mycart.ui.navigation.MyCartNavHost
 import com.asemlab.mycart.ui.theme.MyCartTheme
 
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
+
+            val navController = rememberNavController()
+
             MyCartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    /*.padding(top = 32.dp)*/
+                    bottomBar = { MyCartBottomBar(navController) }
+                ) { innerPadding ->
+
+                    MyCartNavHost(navController, Destination.TABLES, innerPadding)
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCartTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/asemlab/mycart/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.asemlab.mycart
+
+import androidx.lifecycle.ViewModel
+import com.asemlab.mycart.repositories.ProductsRepository
+
+class MainViewModel(private val repository: ProductsRepository) : ViewModel() {
+
+}
